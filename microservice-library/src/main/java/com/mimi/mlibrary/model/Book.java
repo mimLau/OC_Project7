1b17bb9b9e2ec4d707738f95ba21994832893c9e
mimi <lam99@hotmail.fr>
2020-02-19 16:49:54 +0100

    Make Book as a entity. Add attribute author.

--- microservice-library/src/main/java/com/mimi/mlibrary/model/Book.java
+++ microservice-library/src/main/java/com/mimi/mlibrary/model/Book.java
@@ -1,10 +1,24 @@
 package com.mimi.mlibrary.model;
 
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="Books")
 public class Book extends Work {
 
     private String editor;
     private String publishingDate;
     private String title;
+    private String isbn;
+    private Category category;
+
+
+    @ManyToOne
+    @JoinColumn(name="author_fk")
+    private Author author;
 
     public Book() {
         super();
@@ -32,5 +46,29 @@ public class Book extends Work {
 
     public void setTitle(String title) {
         this.title = title;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(Author author) {
+        this.author = author;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
     }
 }
