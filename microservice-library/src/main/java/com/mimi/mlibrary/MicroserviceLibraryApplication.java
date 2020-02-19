5148f1e9a83d6828365de7b784709e51914c8c17
mimi <lam99@hotmail.fr>
2020-02-19 10:08:40 +0100

    Add Application class.

---
+++ microservice-library/src/main/java/com/mimi/mlibrary/MicroserviceLibraryApplication.java
@@ -0,0 +1,13 @@
+package com.mimi.mlibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceLibraryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroserviceLibraryApplication.class, args);
+    }
+
+}
