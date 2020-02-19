727a0fea78d3c56d331e4e90b54416ad7af2449f
mimi <lam99@hotmail.fr>
2020-02-19 12:58:42 +0100

    Create Copy class.

---
+++ microservice-library/src/main/java/com/mimi/mlibrary/model/Copy.java
@@ -0,0 +1,55 @@
+package com.mimi.mlibrary.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="Copies")
+public class Copy {
+
+    private Integer id;
+    private String barcode;
+    private Library library;
+    private boolean available;
+    private String returnDate;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getBarcode() {
+        return barcode;
+    }
+
+    public void setBarcode(String barcode) {
+        this.barcode = barcode;
+    }
+
+    public Library getLibrary() {
+        return library;
+    }
+
+    public void setLibrary(Library library) {
+        this.library = library;
+    }
+
+    public boolean isAvailable() {
+        return available;
+    }
+
+    public void setAvailable(boolean available) {
+        this.available = available;
+    }
+
+    public String getReturnDate() {
+        return returnDate;
+    }
+
+    public void setReturnDate(String returnDate) {
+        this.returnDate = returnDate;
+    }
+}
