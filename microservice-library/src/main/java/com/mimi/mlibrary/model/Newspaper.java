079d62d590a192f481bfe5cec1974d91f79ec006
mimi <lam99@hotmail.fr>
2020-02-19 16:50:56 +0100

    Make Newspaper as an entity.

--- microservice-library/src/main/java/com/mimi/mlibrary/model/Newspaper.java
+++ microservice-library/src/main/java/com/mimi/mlibrary/model/Newspaper.java
@@ -1,5 +1,10 @@
 package com.mimi.mlibrary.model;
 
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="newspapers")
 public class Newspaper extends Work {
 
     private String releaseDate;
