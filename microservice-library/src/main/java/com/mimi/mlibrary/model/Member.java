5135b12703e5c3cbc824094b2bdf9b58452f6f1b
mimi <lam99@hotmail.fr>
2020-02-19 21:34:21 +0100

    Create class Member.

---
+++ microservice-library/src/main/java/com/mimi/mlibrary/model/Member.java
@@ -0,0 +1,28 @@
+package com.mimi.mlibrary.model;
+
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+
+@Entity
+@DiscriminatorValue("Member")
+public class Member extends User {
+
+    private String barcode;
+    private int nbOfCurrentsBorrowings;
+
+    public String getBarcode() {
+        return barcode;
+    }
+
+    public void setBarcode(String barcode) {
+        this.barcode = barcode;
+    }
+
+    public int getNbOfCurrentsBorrowings() {
+        return nbOfCurrentsBorrowings;
+    }
+
+    public void setNbOfCurrentsBorrowings(int nbOfCurrentsBorrowings) {
+        this.nbOfCurrentsBorrowings = nbOfCurrentsBorrowings;
+    }
+}
