8f281c8e85abd9a4f1e4a67cb2085cc7e2834c74
mimi <lam99@hotmail.fr>
2020-02-19 12:47:26 +0100

    Create Review class which inherits from Work class.

---
+++ microservice-library/src/main/java/com/mimi/mlibrary/model/Review.java
@@ -0,0 +1,27 @@
+package com.mimi.mlibrary.model;
+
+public class Review {
+
+    private String releaseDate;
+    private String title;
+
+    public Review() {
+        super();
+    }
+
+    public String getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(String releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
