1729431a3bfae93dd5b14551cc313becd32848c2
mimi <lam99@hotmail.fr>
2020-02-19 13:01:58 +0100

    Create Library class.

---
+++ microservice-library/src/main/java/com/mimi/mlibrary/model/Library.java
@@ -0,0 +1,38 @@
+package com.mimi.mlibrary.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="Libraries")
+public class Library {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+    private String name;
+    private String address;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
