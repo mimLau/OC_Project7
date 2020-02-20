47d4cf4b9e18c50bb50d017e5e0f55cf8f1be831
mimi <lam99@hotmail.fr>
2020-02-20 18:01:06 +0100

    Add AuthorDao object.

--- microservice-library/src/main/java/com/mimi/mlibrary/web/controllers/WorkController.java
+++ microservice-library/src/main/java/com/mimi/mlibrary/web/controllers/WorkController.java
@@ -1,6 +1,8 @@
 package com.mimi.mlibrary.web.controllers;
 
+import com.mimi.mlibrary.dao.AuthorDao;
 import com.mimi.mlibrary.dao.WorkDao;
+import com.mimi.mlibrary.model.works.Author;
 import com.mimi.mlibrary.model.works.Work;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,19 +14,26 @@ import java.util.Optional;
 @RestController
 public class WorkController {
     private WorkDao workDao;
+    private AuthorDao authorDao;
 
-    public WorkController(WorkDao workDao){
+    public WorkController( WorkDao workDao, AuthorDao authorDao ){
         this.workDao = workDao;
+        this.authorDao = authorDao;
     }
 
 
-    @GetMapping(value = "Works")
-    public List<Work> getAllwork() {
+    @GetMapping( value = "/Works" )
+    public List<Work> getAllworks() {
     return workDao.findAll();
     }
 
-    @GetMapping(value = "Work/{id}")
-    public Optional<Work> getWorkById(@PathVariable int id) {
-        return workDao.findById(id);
+    @GetMapping( value = "/Work/{id}" )
+    public Optional<Work> getWorkById( @PathVariable int id ) {
+        return workDao.findById( id );
+    }
+
+    @GetMapping( value = "/Authors" )
+    public List<Author> getAllAuthors() {
+        return authorDao.findAll();
     }
 }
