38ba82afe3f1b7790cd75717afa345dc7a3d3950
mimi <lam99@hotmail.fr>
2020-02-20 10:08:40 +0100

    Remove attributes fistname and lastname and replace with name.

--- microservice-library/src/main/java/com/mimi/mlibrary/model/works/Author.java
+++ microservice-library/src/main/java/com/mimi/mlibrary/model/works/Author.java
@@ -10,9 +10,32 @@ public class Author {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
-    private  String firstname;
-    private String lastname;
+    private  String name;
 
     @OneToMany(mappedBy = "author")
     private List<Book> book;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Book> getBook() {
+        return book;
+    }
+
+    public void setBook(List<Book> book) {
+        this.book = book;
+    }
 }
