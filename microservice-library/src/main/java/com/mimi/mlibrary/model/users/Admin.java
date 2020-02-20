c6abf78751e86c862f8970220790dae41135858d
mimi <lam99@hotmail.fr>
2020-02-20 17:55:08 +0100

    Implements serializable.

--- microservice-library/src/main/java/com/mimi/mlibrary/model/users/Admin.java
+++ microservice-library/src/main/java/com/mimi/mlibrary/model/users/Admin.java
@@ -2,8 +2,9 @@ package com.mimi.mlibrary.model.users;
 
 import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
+import java.io.Serializable;
 
 @Entity
 @DiscriminatorValue("Admin")
-public class Admin extends User {
+public class Admin extends User implements Serializable {
 }
