d93526ee24a0ab3055754859328631e3d02d69c3
mimi <lam99@hotmail.fr>
2020-02-20 17:56:08 +0100

    Implements serializable.

--- microservice-library/src/main/java/com/mimi/mlibrary/model/users/Librarian.java
+++ microservice-library/src/main/java/com/mimi/mlibrary/model/users/Librarian.java
@@ -2,8 +2,9 @@ package com.mimi.mlibrary.model.users;
 
 import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
+import java.io.Serializable;
 
 @Entity
 @DiscriminatorValue("Librarian")
-public class Librarian extends User {
+public class Librarian extends User implements Serializable {
 }
