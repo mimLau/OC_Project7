436abab0d395a4fbdf603f1f6f0401947a1ccdbe
mimi <lam99@hotmail.fr>
2020-02-21 11:43:34 +0100

    Add a list of Borrowings object.

--- microservice-library/src/main/java/com/mimi/mlibrary/model/users/Member.java
+++ microservice-library/src/main/java/com/mimi/mlibrary/model/users/Member.java
@@ -1,8 +1,13 @@
 package com.mimi.mlibrary.model.users;
 
+import com.mimi.mlibrary.model.borrowings.Borrowing;
+
+import javax.persistence.Column;
 import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
+import javax.persistence.OneToMany;
 import java.io.Serializable;
+import java.util.List;
 
 @Entity
 @DiscriminatorValue("Member")
@@ -10,6 +15,9 @@ public class Member extends User implements Serializable {
 
     private String barcode;
     private int nbOfCurrentsBorrowings;
+
+    @OneToMany(mappedBy = "borrowings")
+    private List<Borrowing> borrowings;
 
     public String getBarcode() {
         return barcode;
@@ -25,5 +33,13 @@ public class Member extends User implements Serializable {
 
     public void setNbOfCurrentsBorrowings(int nbOfCurrentsBorrowings) {
         this.nbOfCurrentsBorrowings = nbOfCurrentsBorrowings;
+    }
+
+    public List<Borrowing> getBorrowings() {
+        return borrowings;
+    }
+
+    public void setBorrowings(List<Borrowing> borrowings) {
+        this.borrowings = borrowings;
     }
 }
