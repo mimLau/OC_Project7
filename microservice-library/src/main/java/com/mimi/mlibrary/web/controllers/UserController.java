7b3affd07b425171f520d8c956d8f58aa5ce7842
mimi <lam99@hotmail.fr>
2020-02-22 07:01:45 +0100

    Add addMember method.

--- microservice-library/src/main/java/com/mimi/mlibrary/web/controllers/UserController.java
+++ microservice-library/src/main/java/com/mimi/mlibrary/web/controllers/UserController.java
@@ -3,10 +3,11 @@ package com.mimi.mlibrary.web.controllers;
 import com.mimi.mlibrary.dao.UserDao;
 import com.mimi.mlibrary.model.users.Member;
 import com.mimi.mlibrary.model.users.User;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
@@ -49,5 +50,21 @@ public class UserController {
         return userDao.getMemberByNames( firstname, lastname );
     }
 
+    @PostMapping( value = "/Member/add" )
+    public ResponseEntity<Void> addMember( @RequestBody Member member ) {
+        Member addedMember = userDao.save( member );
+        if( addedMember == null)
+            return ResponseEntity.noContent().build();
+
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand( addedMember.getId() ).toUri();
+        return ResponseEntity.created( location ).build();
+    }
+
+    @PostMapping( value = "/Member/nbBorr/{barcode}" )
+    public void incrementBorrowingsNbBorrowings ( @PathVariable("barcode") String barcode ) {
+         userDao.incrementNbOfCurrentsBorrowings( barcode );
+    }
+
 
 }
+
