9da0b25454048bafffd5d9eac3c0dec9dcb367a5
mimi <lam99@hotmail.fr>
2020-02-22 07:13:38 +0100

    Refactor Work -> Publication

--- microservice-library/src/main/java/com/mimi/mlibrary/dao/WorkDao.java
+++ microservice-library/src/main/java/com/mimi/mlibrary/dao/WorkDao.java
@@ -1,6 +1,6 @@
 package com.mimi.mlibrary.dao;
 
-import com.mimi.mlibrary.model.works.Work;
+import com.mimi.mlibrary.model.works.Publication;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -9,16 +9,16 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
-public interface WorkDao extends JpaRepository<Work, Integer> {
+public interface WorkDao extends JpaRepository<Publication, Integer> {
 
-   @Query("SELECT w FROM Work w WHERE w.isbn= :isbn")
-   Work searchWorkByIsbn( @Param("isbn") String isbn );
+   @Query("SELECT w FROM Publication w WHERE w.isbn= :isbn")
+   Publication searchWorkByIsbn(@Param("isbn") String isbn );
 
-   @Query("SELECT w FROM Work w WHERE w.author.name LIKE %:name%")
-   List<Work> searchWorksByAuthorName( @Param("name") String name );
+   @Query("SELECT w FROM Publication w WHERE w.author.name LIKE %:name%")
+   List<Publication> searchWorksByAuthorName(@Param("name") String name );
 
-   @Query("SELECT w FROM Work w WHERE w.title LIKE %:title%")
-   List<Work> searchWorksByTitle( @Param("title") String title );
+   @Query("SELECT w FROM Publication w WHERE w.title LIKE %:title%")
+   List<Publication> searchWorksByTitle(@Param("title") String title );
 
 
 }
