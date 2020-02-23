647d7f86878bd2a3581ad7556c17a38b85e8829a
mimi <lam99@hotmail.fr>
2020-02-23 10:39:38 +0100

    Add findAll and findById methods.

--- microservice-library/src/main/java/com/mimi/mlibrary/service/PublicationService.java
+++ microservice-library/src/main/java/com/mimi/mlibrary/service/PublicationService.java
@@ -3,11 +3,14 @@ package com.mimi.mlibrary.service;
 import com.mimi.mlibrary.model.publications.Publication;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface PublicationService {
 
+    List<Publication> findAll();
     Publication findByIsbn( String isbn );
     List<Publication> findAllByAuthor( String name );
     List<Publication> findAllByTitle(String title );
+    Optional<Publication> findById( int id);
 
 }
