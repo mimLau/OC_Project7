d271de69edfb3d38e156b1f78c711606c95d5002
mimi <lam99@hotmail.fr>
2020-02-23 10:40:09 +0100

    Add return values to all methods.

--- microservice-library/src/main/java/com/mimi/mlibrary/service/impl/PublicationServiceImpl.java
+++ microservice-library/src/main/java/com/mimi/mlibrary/service/impl/PublicationServiceImpl.java
@@ -3,22 +3,41 @@ package com.mimi.mlibrary.service.impl;
 import com.mimi.mlibrary.dao.PublicationDao;
 import com.mimi.mlibrary.model.publications.Publication;
 import com.mimi.mlibrary.service.PublicationService;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
+@Service
 public class PublicationServiceImpl implements PublicationService {
+
+    private PublicationDao publicationDao;
+
+    PublicationServiceImpl( PublicationDao publicationDao ) {
+        this.publicationDao = publicationDao;
+    }
+
+    public List<Publication> findAll() {
+        return publicationDao.findAll();
+    }
+
+    public Optional<Publication> findById( int id) {
+        return publicationDao.findById( id );
+    }
+
+
     @Override
-    public Publication findByIsbn(String isbn) {
-        return null;
+    public Publication findByIsbn( String isbn ) {
+        return publicationDao.findByIsbn( isbn );
     }
 
     @Override
-    public List<Publication> findAllByAuthor(String name) {
-        return null;
+    public List<Publication> findAllByAuthor( String name ) {
+        return publicationDao.findAllByAuthor( name );
     }
 
     @Override
-    public List<Publication> findAllByTitle(String title) {
-        return null;
+    public List<Publication> findAllByTitle( String title ) {
+        return publicationDao.findAllByTitle( title );
     }
 }
