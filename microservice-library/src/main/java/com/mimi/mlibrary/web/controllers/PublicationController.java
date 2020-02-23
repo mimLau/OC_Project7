a7c59be740f6fe99d2189ed01297346b35eba5a7
mimi <lam99@hotmail.fr>
2020-02-23 10:39:13 +0100

    Use the publication service class instead of dao.

--- microservice-library/src/main/java/com/mimi/mlibrary/web/controllers/PublicationController.java
+++ microservice-library/src/main/java/com/mimi/mlibrary/web/controllers/PublicationController.java
@@ -4,6 +4,7 @@ import com.mimi.mlibrary.dao.AuthorDao;
 import com.mimi.mlibrary.dao.PublicationDao;
 import com.mimi.mlibrary.model.publications.Author;
 import com.mimi.mlibrary.model.publications.Publication;
+import com.mimi.mlibrary.service.PublicationService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,41 +15,40 @@ import java.util.Optional;
 
 @RestController
 public class PublicationController {
-    private PublicationDao publicationDao;
+    private PublicationService publicationService;
     private AuthorDao authorDao;
 
-    public PublicationController(PublicationDao publicationDao, AuthorDao authorDao ){
-        this.publicationDao = publicationDao;
+    public PublicationController(PublicationService publicationService, AuthorDao authorDao ){
+        this.publicationService = publicationService;
         this.authorDao = authorDao;
     }
 
     @GetMapping( value = "/Publication" )
     public List<Publication> getAllPublications(@RequestParam(value="title", required= false) String title) {
         if( title == null )
-            return publicationDao.findAll();
+            return publicationService.findAll();
         else
-            return publicationDao.findAllByTitle(title);
+            return publicationService.findAllByTitle(title);
     }
-
 
     @GetMapping( value = "/Publication/{id}" )
     public Optional<Publication> getPublicationById(@PathVariable int id ) {
-        return publicationDao.findById( id );
+        return publicationService.findById( id );
     }
 
     @GetMapping(value= "/Publication/isbn/{isbn}" )
     public Publication getPublicationByIsbn(@PathVariable String isbn ) {
-       return publicationDao.findByIsbn( isbn );
+       return publicationService.findByIsbn( isbn );
     }
 
     @GetMapping( value ="/Publication/authorName/{name}")
     public List<Publication> getPublicationByAuthor(@PathVariable String name ) {
-        return publicationDao.findAllByAuthor( name );
+        return publicationService.findAllByAuthor( name );
     }
 
     @GetMapping( value ="/Publication/title/{title}")
     public List<Publication> findPublicationByTitle(@PathVariable String title ) {
-        return publicationDao.findAllByTitle( title );
+        return publicationService.findAllByTitle( title );
     }
 
     @GetMapping( value = "/Author" )
