05d3fcb62612c9f4433d04c75e2ecfdc143b26e1
mimi <lam99@hotmail.fr>
2020-02-23 10:41:13 +0100

    Add findAll, findById and save methods.

--- microservice-library/src/main/java/com/mimi/mlibrary/service/UserService.java
+++ microservice-library/src/main/java/com/mimi/mlibrary/service/UserService.java
@@ -1,12 +1,17 @@
 package com.mimi.mlibrary.service;
 
 import com.mimi.mlibrary.model.users.Member;
+import com.mimi.mlibrary.model.users.User;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface UserService {
 
+    List<User> findAll();
+    Optional<User> findById( int id );
     List<Member> getAllMember();
+    Member save(Member member);
     Member getMemberById( Integer id );
     Member getMemberByEmail( String email);
     Member getMemberByNames( String firstname, String lastname );
