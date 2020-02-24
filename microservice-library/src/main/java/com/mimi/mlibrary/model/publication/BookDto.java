7ce81970315a8f13be7a844cb3ea42aede92000d
mimi <lam99@hotmail.fr>
2020-02-24 09:53:46 +0100

    Create BookDto class.

---
+++ microservice-library/src/main/java/com/mimi/mlibrary/model/publication/BookDto.java
@@ -0,0 +1,50 @@
+package com.mimi.mlibrary.model.publication;
+
+public class BookDto extends PublicationDto{
+
+    private String editor;
+    private String publishingDate;
+    private String title;
+    private String isbn;
+    private Category category;
+
+    public String getEditor() {
+        return editor;
+    }
+
+    public void setEditor(String editor) {
+        this.editor = editor;
+    }
+
+    public String getPublishingDate() {
+        return publishingDate;
+    }
+
+    public void setPublishingDate(String publishingDate) {
+        this.publishingDate = publishingDate;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+}
