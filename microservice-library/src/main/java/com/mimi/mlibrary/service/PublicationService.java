8c194e557d354df3310dfad070d16699fbb9eddf
mimi <lam99@hotmail.fr>
2020-02-24 09:52:15 +0100

    Use PublicationDto as return of findall mehtod.

--- microservice-library/src/main/java/com/mimi/mlibrary/service/PublicationService.java
+++ microservice-library/src/main/java/com/mimi/mlibrary/service/PublicationService.java
@@ -1,16 +1,17 @@
 package com.mimi.mlibrary.service;
 
 import com.mimi.mlibrary.model.publication.Publication;
+import com.mimi.mlibrary.model.publication.PublicationDto;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface PublicationService {
 
-    List<Publication> findAll();
+    List<PublicationDto> findAll();
     Publication findByIsbn( String isbn );
     List<Publication> findAllByAuthor( String name );
-    List<Publication> findAllByTitle(String title );
+    List<PublicationDto> findAllByTitle(String title );
     Optional<Publication> findById( int id);
 
 }
