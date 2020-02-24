ec5763d8e2b39608665ead5580614f349eee7c28
mimi <lam99@hotmail.fr>
2020-02-24 09:56:26 +0100

    Create UserDto class.

---
+++ microservice-library/src/main/java/com/mimi/mlibrary/model/user/UserDto.java
@@ -0,0 +1,13 @@
+package com.mimi.mlibrary.model.user;
+
+public class UserDto {
+
+    private Integer id;
+    private String firstname;
+    private String lastname;
+    private String password;
+    private String email;
+    private String phoneNumber;
+    private boolean accountStatus;
+    private String registrationDate;
+}
