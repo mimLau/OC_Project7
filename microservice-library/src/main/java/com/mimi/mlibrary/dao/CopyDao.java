3a24279a69bb9d40081e9c79e793af20d277c26d
mimi <lam99@hotmail.fr>
2020-02-24 09:54:35 +0100

    Create CopyDao interface.

---
+++ microservice-library/src/main/java/com/mimi/mlibrary/dao/CopyDao.java
@@ -0,0 +1,8 @@
+package com.mimi.mlibrary.dao;
+
+import com.mimi.mlibrary.model.publication.Copy;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CopyDao extends JpaRepository<Copy, Integer> {
+
+}
