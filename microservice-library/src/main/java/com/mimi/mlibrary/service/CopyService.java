7192a4229b4ac21ef837bc1992fc31b63fe54fed
mimi <lam99@hotmail.fr>
2020-02-24 09:55:26 +0100

    Create CopyService class.

---
+++ microservice-library/src/main/java/com/mimi/mlibrary/service/CopyService.java
@@ -0,0 +1,10 @@
+package com.mimi.mlibrary.service;
+
+import com.mimi.mlibrary.model.publication.CopyDto;
+
+import java.util.List;
+
+public interface CopyService {
+
+    List<CopyDto> findAll();
+}
