4bcc978e1d084316a19544d9928f22a1aeef6aef
mimi <lam99@hotmail.fr>
2020-02-24 09:55:50 +0100

    Create CopyServiceImpl class.

---
+++ microservice-library/src/main/java/com/mimi/mlibrary/service/impl/CopyServiceImpl.java
@@ -0,0 +1,30 @@
+package com.mimi.mlibrary.service.impl;
+
+import com.mimi.mlibrary.dao.CopyDao;
+import com.mimi.mlibrary.model.mapper.publication.CopyMapper;
+import com.mimi.mlibrary.model.publication.Copy;
+import com.mimi.mlibrary.model.publication.CopyDto;
+import com.mimi.mlibrary.service.CopyService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CopyServiceImpl implements CopyService {
+
+    private CopyDao copyDao;
+    private CopyMapper copyMapper;
+
+    public CopyServiceImpl(CopyDao copyDao, CopyMapper copyMapper) {
+        this.copyDao = copyDao;
+        this.copyMapper = copyMapper;
+    }
+
+    @Override
+    public List<CopyDto> findAll() {
+        List<Copy> copies = copyDao.findAll();
+        List<CopyDto> copyDtos = copyMapper.map( copies );
+
+        return copyDtos;
+    }
+}
