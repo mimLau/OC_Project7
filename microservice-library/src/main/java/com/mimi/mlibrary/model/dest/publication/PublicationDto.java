42940f7c015846f6a3262dd2c19770dc4af47ca8
mimi <lam99@hotmail.fr>
2020-02-25 06:50:44 +0100

    Remove Copies attribute.

--- microservice-library/src/main/java/com/mimi/mlibrary/model/dest/publication/PublicationDto.java
+++ microservice-library/src/main/java/com/mimi/mlibrary/model/dest/publication/PublicationDto.java
@@ -1,11 +1,26 @@
 package com.mimi.mlibrary.model.dest.publication;
 
+import com.mimi.mlibrary.model.source.publication.Copy;
+import com.mimi.mlibrary.model.source.publication.Publication;
+
+import java.util.List;
+
 public class PublicationDto {
 
+    private Integer id;
     private int nbOfAvailableCopies;
     private int nbTotalOfcopies;
-    //private List<Copy> copies;
+
+
     public PublicationDto() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
     }
 
     public int getNbOfAvailableCopies() {
