3e358428f57f0ce4274d1ccf9ccfe8b94adf522c
mimi <lam99@hotmail.fr>
2020-02-25 06:51:08 +0100

    Add PublicationMapper Instance.

--- microservice-library/src/main/java/com/mimi/mlibrary/mapper/publication/PublicationMapper.java
+++ microservice-library/src/main/java/com/mimi/mlibrary/mapper/publication/PublicationMapper.java
@@ -2,13 +2,20 @@ package com.mimi.mlibrary.mapper.publication;
 
 import com.mimi.mlibrary.model.source.publication.Publication;
 import com.mimi.mlibrary.model.dest.publication.PublicationDto;
+import org.mapstruct.InheritInverseConfiguration;
 import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
 
 import java.util.List;
 
 @Mapper
 public interface PublicationMapper {
 
+    PublicationMapper INSTANCE = Mappers.getMapper( PublicationMapper.class);
     PublicationDto map( Publication publication );
     List <PublicationDto> map( List<Publication> publications );
+
+
+
+
 }
