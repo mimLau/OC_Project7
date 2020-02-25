3215f5a998a87c92e91d8633ebb2b397607644e1
mimi <lam99@hotmail.fr>
2020-02-25 08:37:24 +0100

    Add extension to JpaRep

--- microservice-library/src/main/java/com/mimi/mlibrary/dao/BorrowingDao.java
+++ microservice-library/src/main/java/com/mimi/mlibrary/dao/BorrowingDao.java
@@ -1,4 +1,12 @@
 package com.mimi.mlibrary.dao;
 
-public interface BorrowingDao {
+import com.mimi.mlibrary.model.source.borrowing.Borrowing;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface BorrowingDao extends JpaRepository<Borrowing, Integer> {
+
 }
