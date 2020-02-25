9c0358a30ff5f4017131a45eb56afaa6de6dd25e
mimi <lam99@hotmail.fr>
2020-02-25 06:43:53 +0100

    add inheritance conf.

--- microservice-library/src/main/java/com/mimi/mlibrary/mapper/publication/BookMapper.java
+++ microservice-library/src/main/java/com/mimi/mlibrary/mapper/publication/BookMapper.java
@@ -1,11 +1,19 @@
 package com.mimi.mlibrary.mapper.publication;
 
+import com.mimi.mlibrary.model.dest.publication.CopyDto;
 import com.mimi.mlibrary.model.source.publication.Book;
 import com.mimi.mlibrary.model.dest.publication.BookDto;
+import com.mimi.mlibrary.model.source.publication.Copy;
+import org.mapstruct.InheritConfiguration;
 import org.mapstruct.Mapper;
 
-@Mapper
+import java.util.List;
+
+@Mapper( config = BookMapperConfig.class )
 public interface BookMapper {
 
+    @InheritConfiguration( name = "mapBook")
     BookDto map( Book book );
+    List<BookDto> map( List<Book> books );
+
 }
