0e1dfc2440a05c68baca411d1dcfda6ac2fd015f
mimi <lam99@hotmail.fr>
2020-02-25 16:45:30 +0100

    Create MemberService class.

---
+++ microservice-library/src/main/java/com/mimi/mlibrary/service/MemberService.java
@@ -0,0 +1,17 @@
+package com.mimi.mlibrary.service;
+
+import com.mimi.mlibrary.model.source.account.MemberAccount;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface MemberService {
+
+    List<MemberAccount> findAll();
+    Optional<MemberAccount> findById( int id );
+    MemberAccount save(MemberAccount Member);
+    MemberAccount getMemberById( Integer id );
+    MemberAccount getMemberByEmail( String email);
+    MemberAccount getMemberByNames( String firstname, String lastname );
+    void incrementNbOfCurrentsBorrowings( String barcode );
+}
