e7b5fd23a9220f44a75c6a1b26f523a0c4e21c4c
mimi <lam99@hotmail.fr>
2020-02-25 08:37:05 +0100

    Add getAllBorrowings method.

--- microservice-library/src/main/java/com/mimi/mlibrary/web/controllers/BorrowingController.java
+++ microservice-library/src/main/java/com/mimi/mlibrary/web/controllers/BorrowingController.java
@@ -1,7 +1,23 @@
 package com.mimi.mlibrary.web.controllers;
 
+import com.mimi.mlibrary.model.dest.borrowing.BorrowingDto;
+import com.mimi.mlibrary.service.BorrowingService;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class BorrowingController {
+
+    private BorrowingService borrowingService;
+
+    public BorrowingController(BorrowingService borrowingService) {
+        this.borrowingService = borrowingService;
+    }
+
+    @GetMapping( value = "/Borrowing")
+    public List<BorrowingDto> getAllBorrowings() {
+        return borrowingService.findAll();
+    }
 }
