51c0dbd021e45bf503f2d04da18cd4bea1e7c4b2
mimi <lam99@hotmail.fr>
2020-02-25 08:37:38 +0100

    Create BorrowingDto.

---
+++ microservice-library/src/main/java/com/mimi/mlibrary/model/dest/borrowing/BorrowingDto.java
@@ -0,0 +1,70 @@
+package com.mimi.mlibrary.model.dest.borrowing;
+
+import com.mimi.mlibrary.model.source.borrowing.BorrowingStatus;
+import com.mimi.mlibrary.model.source.publication.Copy;
+import com.mimi.mlibrary.model.source.user.Member;
+
+
+public class BorrowingDto {
+
+    private Integer id;
+
+    private String returnDate;
+    private String borrowingDate;
+    private int reminderNb;
+    private boolean extented;
+    private BorrowingStatus borrowingStatus;
+    //private Copy copy;
+    //private Member member;
+
+    public BorrowingDto() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getReturnDate() {
+        return returnDate;
+    }
+
+    public void setReturnDate(String returnDate) {
+        this.returnDate = returnDate;
+    }
+
+    public String getBorrowingDate() {
+        return borrowingDate;
+    }
+
+    public void setBorrowingDate(String borrowingDate) {
+        this.borrowingDate = borrowingDate;
+    }
+
+    public int getReminderNb() {
+        return reminderNb;
+    }
+
+    public void setReminderNb(int reminderNb) {
+        this.reminderNb = reminderNb;
+    }
+
+    public boolean isExtented() {
+        return extented;
+    }
+
+    public void setExtented(boolean extented) {
+        this.extented = extented;
+    }
+
+    public BorrowingStatus getBorrowingStatus() {
+        return borrowingStatus;
+    }
+
+    public void setBorrowingStatus(BorrowingStatus borrowingStatus) {
+        this.borrowingStatus = borrowingStatus;
+    }
+}
