988c9c19000cc9c561fdf2174ab2c907e55d0ae5
mimi <lam99@hotmail.fr>
2020-02-25 06:45:07 +0100

    Create BookMapperConfig to configure inheritance between Book and Publication.

---
+++ microservice-library/src/main/java/com/mimi/mlibrary/mapper/publication/BookMapperConfig.java
@@ -0,0 +1,14 @@
+package com.mimi.mlibrary.mapper.publication;
+
+import com.mimi.mlibrary.model.dest.publication.BookDto;
+import com.mimi.mlibrary.model.source.publication.Book;
+import org.mapstruct.InheritConfiguration;
+import org.mapstruct.MapperConfig;
+import org.mapstruct.MappingTarget;
+
+@MapperConfig
+public interface BookMapperConfig extends PublicationMapperConfig {
+
+    @InheritConfiguration( name = "mapPublication")
+    void mapBook(Book book, @MappingTarget BookDto bookDto);
+}
