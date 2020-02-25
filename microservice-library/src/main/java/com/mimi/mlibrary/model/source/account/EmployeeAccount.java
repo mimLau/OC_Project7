30c363d37030aa4e1f330ac1d0d7bc665a402407
mimi <lam99@hotmail.fr>
2020-02-25 16:25:37 +0100

    Create EmployeeAccount class.

---
+++ microservice-library/src/main/java/com/mimi/mlibrary/model/source/account/EmployeeAccount.java
@@ -0,0 +1,19 @@
+package com.mimi.mlibrary.model.source.account;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table( name = "Employees")
+public abstract class EmployeeAccount extends Account {
+
+    private Role role;
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+}
