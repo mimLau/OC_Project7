abbb0870942848bc07117d6c49b0fb6b8dfd9aeb
mimi <lam99@hotmail.fr>
2020-02-25 08:40:50 +0100

    Add findAll method.

--- microservice-library/src/main/java/com/mimi/mlibrary/service/BorrowingService.java
+++ microservice-library/src/main/java/com/mimi/mlibrary/service/BorrowingService.java
@@ -1,4 +1,10 @@
 package com.mimi.mlibrary.service;
 
+import com.mimi.mlibrary.model.dest.borrowing.BorrowingDto;
+
+import java.util.List;
+
 public interface BorrowingService {
+
+    List<BorrowingDto> findAll();
 }
