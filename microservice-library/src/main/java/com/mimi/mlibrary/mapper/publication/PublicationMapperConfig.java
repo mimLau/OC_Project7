3854d241f21589d746ccd4da4f9e7416b33780ae
mimi <lam99@hotmail.fr>
2020-02-25 06:51:38 +0100

    Create PublicationMapperConfig for the inheritance configuration.

---
+++ microservice-library/src/main/java/com/mimi/mlibrary/mapper/publication/PublicationMapperConfig.java
@@ -0,0 +1,12 @@
+package com.mimi.mlibrary.mapper.publication;
+
+import com.mimi.mlibrary.model.dest.publication.PublicationDto;
+import com.mimi.mlibrary.model.source.publication.Publication;
+import org.mapstruct.MapperConfig;
+import org.mapstruct.MappingTarget;
+
+@MapperConfig
+public interface PublicationMapperConfig {
+
+    void mapPublication(Publication mean, @MappingTarget PublicationDto publicationDto);
+}
