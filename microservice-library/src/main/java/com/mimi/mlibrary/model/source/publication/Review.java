88ed415630f71a6fc3f6a54f4eeabb520d8d75d6
mimi <lam99@hotmail.fr>
2020-02-26 06:42:11 +0100

    Delete DiscriminatorValue.

--- microservice-library/src/main/java/com/mimi/mlibrary/model/source/publication/Review.java
+++ microservice-library/src/main/java/com/mimi/mlibrary/model/source/publication/Review.java
@@ -1,15 +1,21 @@
 package com.mimi.mlibrary.model.source.publication;
 
-import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
 import java.io.Serializable;
+import java.util.List;
 
 @Entity
-@DiscriminatorValue("Review")
+@Table( name = "Reviews")
 public class Review extends Publication implements Serializable {
 
     private String releaseDate;
     private String name;
+
+    @OneToMany(mappedBy = "review")
+    private List<Copy> copies;
 
     public Review() {
         super();
@@ -29,5 +35,13 @@ public class Review extends Publication implements Serializable {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public List<Copy> getCopies() {
+        return copies;
+    }
+
+    public void setCopies(List<Copy> copies) {
+        this.copies = copies;
     }
 }
