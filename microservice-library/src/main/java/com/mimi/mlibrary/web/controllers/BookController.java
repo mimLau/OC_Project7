19c77244eba00a9241e391c8b605a2e843f1569c
mimi <lam99@hotmail.fr>
2020-02-26 12:47:52 +0100

    Use @RequestParam

--- microservice-library/src/main/java/com/mimi/mlibrary/web/controllers/BookController.java
+++ microservice-library/src/main/java/com/mimi/mlibrary/web/controllers/BookController.java
@@ -4,6 +4,7 @@ import com.mimi.mlibrary.model.source.publication.Book;
 import com.mimi.mlibrary.service.publication.BookService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -13,22 +14,22 @@ public class BookController {
 
     private BookService bookService;
 
-    public BookController(BookService bookService) {
+    public BookController( BookService bookService ) {
         this.bookService = bookService;
     }
 
-    @GetMapping( value = "/Books")
+    @GetMapping( value = "/Books" )
     public List<Book> getAllBooks() {
         return bookService.findAll();
     }
 
-    @GetMapping( value = "/Books/{name}")
-    public List<Book> findAllByAuthor(@PathVariable String name) {
+    @GetMapping( value = "/Books", params = "name" )
+    public List<Book> findAllByAuthor( @RequestParam String name ) {
         return bookService.findAllByAuthor( name );
     }
 
-    @GetMapping( value = "/Books/{title}")
-    public List<Book> findAllByTitle(@PathVariable String title) {
+    @GetMapping( value = "/Books/", params = "title")
+    public List<Book> findAllByTitle( @RequestParam String title ) {
         return bookService.findAllByTitle( title );
     }
 }
