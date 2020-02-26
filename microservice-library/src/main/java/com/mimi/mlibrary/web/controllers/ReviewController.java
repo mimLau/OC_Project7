7ecd70728486afca32d94aeb6921465835f56f8e
mimi <lam99@hotmail.fr>
2020-02-26 13:31:15 +0100

    Add findAll, findAllByName and findAllByDate methods.

--- microservice-library/src/main/java/com/mimi/mlibrary/web/controllers/ReviewController.java
+++ microservice-library/src/main/java/com/mimi/mlibrary/web/controllers/ReviewController.java
@@ -1,4 +1,37 @@
 package com.mimi.mlibrary.web.controllers;
 
+import com.mimi.mlibrary.model.source.publication.Newspaper;
+import com.mimi.mlibrary.model.source.publication.Review;
+import com.mimi.mlibrary.service.publication.ReviewService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
 public class ReviewController {
+
+    private ReviewService reviewService;
+
+
+    public ReviewController(ReviewService reviewService) {
+        this.reviewService = reviewService;
+    }
+
+
+    @GetMapping( value = "/Reviews")
+    public List<Review> findAll(){
+        return reviewService.findAll();
+    }
+
+    @GetMapping( value = "/Reviews", params = "name")
+    public List<Review> findAllByName( @RequestParam String name ){
+        return reviewService.findAllByName( name );
+    }
+
+    @GetMapping( value = "/Reviews", params = "date")
+    public List<Review> findAllByDate( @RequestParam String date ){
+        return reviewService.findAllByDate( date );
+    }
 }
