82e9d11d565f955ac4fa5f9874fca11ebd021470
mimi <lam99@hotmail.fr>
2020-02-26 13:27:35 +0100

    Implement findAll, findAllByName and findAllByDate methods.

--- microservice-library/src/main/java/com/mimi/mlibrary/service/impl/publication/ReviewSericeImpl.java
+++ microservice-library/src/main/java/com/mimi/mlibrary/service/impl/publication/ReviewSericeImpl.java
@@ -1,8 +1,33 @@
 package com.mimi.mlibrary.service.impl.publication;
 
+import com.mimi.mlibrary.dao.publication.ReviewDao;
+import com.mimi.mlibrary.model.source.publication.Review;
 import com.mimi.mlibrary.service.publication.ReviewService;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class ReviewSericeImpl implements ReviewService {
+
+    private ReviewDao reviewDao;
+
+    public ReviewSericeImpl(ReviewDao reviewDao) {
+        this.reviewDao = reviewDao;
+    }
+
+    @Override
+    public List<Review> findAll() {
+        return reviewDao.findAll();
+    }
+
+    @Override
+    public List<Review> findAllByName( String name ) {
+        return reviewDao.findAllByName( name );
+    }
+
+    @Override
+    public List<Review> findAllByDate( String date ) {
+        return reviewDao.findAllByDate( date );
+    }
 }
