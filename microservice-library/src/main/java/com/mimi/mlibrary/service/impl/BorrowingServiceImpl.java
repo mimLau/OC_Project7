e53872b8ed3ce4ad0c1fe8637dbc7604748039eb
mimi <lam99@hotmail.fr>
2020-02-26 07:52:01 +0100

    Override findByMemberId method.

--- microservice-library/src/main/java/com/mimi/mlibrary/service/impl/BorrowingServiceImpl.java
+++ microservice-library/src/main/java/com/mimi/mlibrary/service/impl/BorrowingServiceImpl.java
@@ -13,17 +13,22 @@ import java.util.List;
 public class BorrowingServiceImpl implements BorrowingService {
 
     private BorrowingDao borrowingDao;
-    private BorrowingMapper borrowingMapper;
+    //private BorrowingMapper borrowingMapper;
 
     public BorrowingServiceImpl(BorrowingDao borrowingDao, BorrowingMapper borrowingMapper, BorrowingMapper borrowingMapper1) {
         this.borrowingDao = borrowingDao;
-        this.borrowingMapper = borrowingMapper1;
+        //this.borrowingMapper = borrowingMapper1;
     }
 
     @Override
+    public List<Borrowing> findByMemberId(int memberId) {
+        return borrowingDao.findByMemberId( memberId );
+    }
+
+    /*@Override
     public List<BorrowingDto> findAll() {
         List <Borrowing> borrowings = borrowingDao.findAll();
         List <BorrowingDto> borrowingDtos = borrowingMapper.map( borrowings );
         return borrowingDtos;
-    }
+    }*/
 }
