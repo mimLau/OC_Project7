bc9798b05471a758bea237e55417c01f22753169
mimi <lam99@hotmail.fr>
2020-02-26 13:24:27 +0100

    Make ReviewDao as A Repository. Add findAllByName and findAllByDate.

--- microservice-library/src/main/java/com/mimi/mlibrary/dao/publication/ReviewDao.java
+++ microservice-library/src/main/java/com/mimi/mlibrary/dao/publication/ReviewDao.java
@@ -1,4 +1,19 @@
 package com.mimi.mlibrary.dao.publication;
 
-public interface ReviewDao {
+import com.mimi.mlibrary.model.source.publication.Review;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ReviewDao extends JpaRepository<Review, Integer> {
+
+    @Query("SELECT r FROM Review r WHERE r.name= :name")
+    List<Review> findAllByName( @Param("name") String name );
+
+    @Query("SELECT r FROM Review r WHERE r.releaseDate= :date")
+    List<Review> findAllByDate( @Param("date") String date );
 }
