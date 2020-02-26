5371f7a23f768284ae96da3d41ecd0b32d40eac0
mimi <lam99@hotmail.fr>
2020-02-26 13:01:28 +0100

    Add findAllByMemberId.

--- microservice-library/src/main/java/com/mimi/mlibrary/web/controllers/BorrowingController.java
+++ microservice-library/src/main/java/com/mimi/mlibrary/web/controllers/BorrowingController.java
@@ -1,8 +1,10 @@
 package com.mimi.mlibrary.web.controllers;
 
-import com.mimi.mlibrary.model.dest.borrowing.BorrowingDto;
-import com.mimi.mlibrary.service.BorrowingService;
+import com.mimi.mlibrary.model.source.borrowing.Borrowing;
+import com.mimi.mlibrary.service.borrowing.BorrowingService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -12,12 +14,17 @@ public class BorrowingController {
 
     private BorrowingService borrowingService;
 
-    public BorrowingController(BorrowingService borrowingService) {
+    public BorrowingController( BorrowingService borrowingService ) {
         this.borrowingService = borrowingService;
     }
 
-    @GetMapping( value = "/Borrowing")
-    public List<BorrowingDto> getAllBorrowings() {
+    @GetMapping( value = "/Borrowings/MemberId", params = "id")
+    public List<Borrowing> findAllByUserId( @RequestParam int id ) {
+        return borrowingService.findByMemberId( id );
+    }
+
+    @GetMapping( value = "/Borrowings")
+    public List<Borrowing> getAllBorrowings() {
         return borrowingService.findAll();
     }
 }
