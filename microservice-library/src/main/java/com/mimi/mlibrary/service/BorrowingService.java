486f25d22ad330276e914247b882457001ff6cb3
mimi <lam99@hotmail.fr>
2020-02-26 07:51:49 +0100

    Add findByMemberId method.

--- microservice-library/src/main/java/com/mimi/mlibrary/service/BorrowingService.java
+++ microservice-library/src/main/java/com/mimi/mlibrary/service/BorrowingService.java
@@ -1,10 +1,12 @@
 package com.mimi.mlibrary.service;
 
 import com.mimi.mlibrary.model.dest.borrowing.BorrowingDto;
+import com.mimi.mlibrary.model.source.borrowing.Borrowing;
 
 import java.util.List;
 
 public interface BorrowingService {
 
-    List<BorrowingDto> findAll();
+    //List<BorrowingDto> findAll();
+    List<Borrowing> findByMemberId( int memberId );
 }
