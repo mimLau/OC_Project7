f67996d688a6fcb135773372a9477b11191bdff1
mimi <lam99@hotmail.fr>
2020-02-26 07:51:09 +0100

    Add findByMemberId method.

--- microservice-library/src/main/java/com/mimi/mlibrary/dao/BorrowingDao.java
+++ microservice-library/src/main/java/com/mimi/mlibrary/dao/BorrowingDao.java
@@ -2,11 +2,16 @@ package com.mimi.mlibrary.dao;
 
 import com.mimi.mlibrary.model.source.borrowing.Borrowing;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface BorrowingDao extends JpaRepository<Borrowing, Integer> {
+
+    @Query("SELECT b FROM Borrowing  b JOIN FETCH b.member m WHERE m.id= :id")
+    List<Borrowing> findByMemberId(@Param("id") int id);
 
 }
