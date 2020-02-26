d74bb27e2ba4983734334fef0ec4ec5430358a1e
mimi <lam99@hotmail.fr>
2020-02-26 13:25:57 +0100

    Add findAll, findAllByName and findAllByDate methods.

--- microservice-library/src/main/java/com/mimi/mlibrary/service/publication/ReviewService.java
+++ microservice-library/src/main/java/com/mimi/mlibrary/service/publication/ReviewService.java
@@ -1,4 +1,12 @@
 package com.mimi.mlibrary.service.publication;
 
+import com.mimi.mlibrary.model.source.publication.Review;
+
+import java.util.List;
+
 public interface ReviewService {
+
+    List<Review> findAll();
+    List<Review> findAllByName( String name );
+    List<Review> findAllByDate(String date );
 }
