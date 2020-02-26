fcb8194a7cd88dc9e8ef3abf05319a770a026e36
mimi <lam99@hotmail.fr>
2020-02-26 07:49:01 +0100

    Add findAllByAuthor, and findAllByTitle methods.

--- microservice-library/src/main/java/com/mimi/mlibrary/web/controllers/BookController.java
+++ microservice-library/src/main/java/com/mimi/mlibrary/web/controllers/BookController.java
@@ -3,6 +3,7 @@ package com.mimi.mlibrary.web.controllers;
 import com.mimi.mlibrary.model.source.publication.Book;
 import com.mimi.mlibrary.service.BookService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -19,5 +20,15 @@ public class BookController {
     @GetMapping( value = "/Book")
     public List<Book> getAllBooks() {
         return bookService.findAll();
+    }
+
+    @GetMapping( value = "/BookByAuthor/{name}")
+    public List<Book> findAllByAuthor(@PathVariable String name) {
+        return bookService.findAllByAuthor( name );
+    }
+
+    @GetMapping( value = "/BookByTitle/{title}")
+    public List<Book> findAllByTitle(@PathVariable String title) {
+        return bookService.findAllByTitle( title );
     }
 }
