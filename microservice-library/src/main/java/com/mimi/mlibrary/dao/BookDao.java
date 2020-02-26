5fa188677073d444c6b4c513594ea6965767cb11
mimi <lam99@hotmail.fr>
2020-02-26 07:49:29 +0100

    Add findAllByAuthor and findAllByTitle methods.

--- microservice-library/src/main/java/com/mimi/mlibrary/dao/BookDao.java
+++ microservice-library/src/main/java/com/mimi/mlibrary/dao/BookDao.java
@@ -3,6 +3,7 @@ package com.mimi.mlibrary.dao;
 import com.mimi.mlibrary.model.source.publication.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,4 +11,9 @@ import java.util.List;
 @Repository
 public interface BookDao extends JpaRepository<Book, Integer> {
 
+    @Query("SELECT b FROM Book b JOIN FETCH b.author a WHERE a.firstname= :name OR a.lastname= :name or a.alias= :name")
+    List<Book> findAllByAuthor(@Param("name") String name);
+
+    @Query("SELECT b FROM Book b WHERE b.title LIKE %:title%")
+    List<Book> findAllByTitle( @Param("title") String title );
 }
