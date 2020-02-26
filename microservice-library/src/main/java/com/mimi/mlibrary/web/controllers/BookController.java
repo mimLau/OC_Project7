0c308ae7e399368312184f5faa1d4ea53bb083d1
mimi <lam99@hotmail.fr>
2020-02-26 08:03:20 +0100

    Modify package.

--- microservice-library/src/main/java/com/mimi/mlibrary/web/controllers/BookController.java
+++ microservice-library/src/main/java/com/mimi/mlibrary/web/controllers/BookController.java
@@ -1,7 +1,7 @@
 package com.mimi.mlibrary.web.controllers;
 
 import com.mimi.mlibrary.model.source.publication.Book;
-import com.mimi.mlibrary.service.BookService;
+import com.mimi.mlibrary.service.publication.BookService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
