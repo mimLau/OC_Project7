bc273c57173b818ac1bb4a592fc5d7927c9a0f22
mimi <lam99@hotmail.fr>
2020-02-26 11:18:23 +0100

    Create NewspaperController class.

---
+++ microservice-library/src/main/java/com/mimi/mlibrary/web/controllers/NewspaperController.java
@@ -0,0 +1,27 @@
+package com.mimi.mlibrary.web.controllers;
+
+import com.mimi.mlibrary.model.source.publication.Newspaper;
+import com.mimi.mlibrary.service.publication.NewspaperService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+
+@RestController
+public class NewspaperController {
+
+    private NewspaperService newspaperService;
+
+    public NewspaperController(NewspaperService newspaperService) {
+        this.newspaperService = newspaperService;
+    }
+
+    @GetMapping( value = "/NewspaperByName/{name}" )
+    public List<Newspaper> findAllByName(@PathVariable String name ){
+        return newspaperService.findAllByName( name );
+    }
+}
+
+
