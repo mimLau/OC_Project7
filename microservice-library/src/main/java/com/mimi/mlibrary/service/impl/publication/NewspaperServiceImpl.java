7682e2f0fa4f6dfe03de8dbf571034b689eb30b0
mimi <lam99@hotmail.fr>
2020-02-26 13:18:07 +0100

    Override findAll and findAllByDate methods.

--- microservice-library/src/main/java/com/mimi/mlibrary/service/impl/publication/NewspaperServiceImpl.java
+++ microservice-library/src/main/java/com/mimi/mlibrary/service/impl/publication/NewspaperServiceImpl.java
@@ -3,6 +3,7 @@ package com.mimi.mlibrary.service.impl.publication;
 import com.mimi.mlibrary.dao.publication.NewspaperDao;
 import com.mimi.mlibrary.model.source.publication.Newspaper;
 import com.mimi.mlibrary.service.publication.NewspaperService;
+import com.sun.xml.bind.v2.TODO;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -12,12 +13,24 @@ public class NewspaperServiceImpl implements NewspaperService {
 
     private NewspaperDao newspaperDao;
 
-    public NewspaperServiceImpl(NewspaperDao newspaperDao) {
+    public NewspaperServiceImpl( NewspaperDao newspaperDao ) {
         this.newspaperDao = newspaperDao;
     }
 
     @Override
-    public List<Newspaper> findAllByName(String name) {
+    public List<Newspaper> findAll() {
+        return newspaperDao.findAll();
+    }
+
+    @Override
+    public List<Newspaper> findAllByName( String name ) {
         return newspaperDao.findAllByName( name );
+    }
+
+    @Override
+    public List<Newspaper> findAllByDate( String date ) {
+
+        //TODO convert date or something like this
+        return newspaperDao.findAllByDate( date );
     }
 }
