289e8506cc48f5a3a66cafae54734b13b6266aa0
mimi <lam99@hotmail.fr>
2020-02-26 13:21:17 +0100

    Create ReviewServiceImpl and make it as a Service.

---
+++ microservice-library/src/main/java/com/mimi/mlibrary/service/impl/publication/ReviewSericeImpl.java
@@ -0,0 +1,8 @@
+package com.mimi.mlibrary.service.impl.publication;
+
+import com.mimi.mlibrary.service.publication.ReviewService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ReviewSericeImpl implements ReviewService {
+}
