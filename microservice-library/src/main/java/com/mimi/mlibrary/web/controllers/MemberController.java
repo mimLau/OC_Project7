b5701b4a36e4558b95a897d01ae970cc5fb2a59e
mimi <lam99@hotmail.fr>
2020-02-27 07:58:59 +0100

    Rectify typo errors.

--- microservice-library/src/main/java/com/mimi/mlibrary/web/controllers/MemberController.java
+++ microservice-library/src/main/java/com/mimi/mlibrary/web/controllers/MemberController.java
@@ -14,36 +14,36 @@ import java.util.Optional;
 @RestController
 public class MemberController {
 
-    private MemberService MemberService;
+    private MemberService memberService;
 
     MemberController(MemberServiceImpl MemberService ) {
-        this.MemberService = MemberService;
+        this.memberService = memberService;
     }
 
     @GetMapping( value = "/Members" )
     public List<MemberAccount> getAllMembers() {
-        return MemberService.findAll();
+        return memberService.findAll();
     }
 
     @GetMapping( value = "/Members", params = "id" )
     public Optional<MemberAccount> getMemberById( @RequestParam("id") int id ) {
-        return MemberService.findById( id );
+        return memberService.findById( id );
     }
 
 
     @GetMapping( value = "/Members", params = "email")
     public MemberAccount getMemberByEmail ( @RequestParam("email") String email ) {
-        return MemberService.getMemberByEmail(email);
+        return memberService.getMemberByEmail(email);
     }
 
     @GetMapping( value = "/Members", params = { "firstname", "lastname" })
     public MemberAccount getMemberByName ( @RequestParam("firstname") String firstname, @RequestParam("lastname") String lastname ) {
-        return MemberService.getMemberByNames( firstname, lastname );
+        return memberService.getMemberByNames( firstname, lastname );
     }
 
-    @PostMapping( value = "/Member/add" )
+    @PostMapping( value = "/Members/add" )
     public ResponseEntity<Void> addMember( @RequestBody MemberAccount Member ) {
-        MemberAccount addedMember = MemberService.save( Member );
+        MemberAccount addedMember = memberService.save( Member );
         if( addedMember == null)
             return ResponseEntity.noContent().build();
 
@@ -53,7 +53,7 @@ public class MemberController {
 
     @PostMapping( value = "/Member/nbBorr/{barcode}" )
     public void incrementBorrowingsNbBorrowings ( @PathVariable("barcode") String barcode ) {
-        MemberService.incrementNbOfCurrentsBorrowings( barcode );
+        memberService.incrementNbOfCurrentsBorrowings( barcode );
     }
 
 }
