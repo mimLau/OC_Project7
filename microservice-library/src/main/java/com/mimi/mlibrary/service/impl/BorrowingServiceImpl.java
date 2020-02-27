67663918ea60356db9284938b01502f27f8d1f5e
mimi <lam99@hotmail.fr>
2020-02-27 20:38:32 +0100

    Features:
    Add findBorrowingById and updateBorrowingReturnDateById methods.

--- microservice-library/src/main/java/com/mimi/mlibrary/service/impl/BorrowingServiceImpl.java
+++ microservice-library/src/main/java/com/mimi/mlibrary/service/impl/BorrowingServiceImpl.java
@@ -4,9 +4,11 @@ import com.mimi.mlibrary.dao.borrowing.BorrowingDao;
 import com.mimi.mlibrary.mapper.borrowing.BorrowingMapper;
 import com.mimi.mlibrary.model.source.borrowing.Borrowing;
 import com.mimi.mlibrary.service.BorrowingService;
+import org.joda.time.LocalDate;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class BorrowingServiceImpl implements BorrowingService {
@@ -17,6 +19,12 @@ public class BorrowingServiceImpl implements BorrowingService {
     public BorrowingServiceImpl(BorrowingDao borrowingDao, BorrowingMapper borrowingMapper, BorrowingMapper borrowingMapper1) {
         this.borrowingDao = borrowingDao;
         //this.borrowingMapper = borrowingMapper1;
+    }
+
+
+    @Override
+    public Borrowing findBorrowingById( int id ) {
+        return borrowingDao.findBorrowingById( id );
     }
 
     @Override
@@ -32,6 +40,21 @@ public class BorrowingServiceImpl implements BorrowingService {
     @Override
     public Borrowing save( Borrowing borrowing ) {
         return borrowingDao.save( borrowing );
+    }
+
+    @Override
+    public void updateBorrowingReturnDateById( int extendedDay, int id ) {
+
+       //Retrieve a borrowing by its id
+       Borrowing borrowing = borrowingDao.findBorrowingById( id );
+
+       //Retrieve the return date of this borrowing
+       LocalDate returnDate = borrowing.getReturnDate();
+
+       //Extend the return date by 4 weeks
+       returnDate = returnDate.plusDays( extendedDay );
+
+       borrowingDao.updateBorrowingReturnDateById( returnDate, id );
     }
 
 
