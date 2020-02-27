181dde207725b6ca6e7089e71e6525d7883233a6
mimi <lam99@hotmail.fr>
2020-02-27 07:56:50 +0100

    Add several methods.

--- microservice-library/src/main/java/com/mimi/mlibrary/service/publication/CopyService.java
+++ microservice-library/src/main/java/com/mimi/mlibrary/service/publication/CopyService.java
@@ -1,10 +1,17 @@
 package com.mimi.mlibrary.service.publication;
 
-import com.mimi.mlibrary.model.dest.publication.CopyDto;
+import com.mimi.mlibrary.model.source.publication.Copy;
+import org.joda.time.LocalDate;
 
 import java.util.List;
 
 public interface CopyService {
 
-    List<CopyDto> findAll();
+    //List<CopyDto> findAll();
+
+    List<Copy> findAll();
+    List<Copy> findAllByBookId( int id );
+    List<Copy> findAllByReviewId( int id );
+    List<Copy> findAllByNewspaperId( int id );
+    List<Copy> findAllByDelay( );
 }
