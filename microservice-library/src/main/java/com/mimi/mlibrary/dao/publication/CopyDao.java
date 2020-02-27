fef2562baeaa62aa641fdd2e4a7752a4aa78118e
mimi <lam99@hotmail.fr>
2020-02-27 07:56:30 +0100

    Add several methods.

--- microservice-library/src/main/java/com/mimi/mlibrary/dao/publication/CopyDao.java
+++ microservice-library/src/main/java/com/mimi/mlibrary/dao/publication/CopyDao.java
@@ -1,8 +1,25 @@
 package com.mimi.mlibrary.dao.publication;
 
 import com.mimi.mlibrary.model.source.publication.Copy;
+import org.joda.time.LocalDate;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
 
 public interface CopyDao extends JpaRepository<Copy, Integer> {
 
+    @Query("Select  c FROM Copy c JOIN FETCH c.book b where b.id= :id")
+    List<Copy> findAllByBookId( @Param( "id" ) int id );
+
+    @Query("Select  c FROM Copy c JOIN FETCH c.review b where b.id= :id")
+    List<Copy> findAllByReviewId( @Param( "id" ) int id );
+
+    @Query("Select  c FROM Copy c JOIN FETCH c.newspaper b where b.id= :id")
+    List<Copy> findAllByNewspaperId( @Param( "id" ) int id );
+
+    @Query("SELECT c FROM Copy c WHERE c.returnDate < :today")
+    List<Copy> findAllByDelay( @Param("today") LocalDate currentDate );
 }
