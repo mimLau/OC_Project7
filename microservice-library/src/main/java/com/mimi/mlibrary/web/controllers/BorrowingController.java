6b5ba50479d424a6520673fbd6feaf0557328cd8
mimi <lam99@hotmail.fr>
2020-02-27 07:51:40 +0100

    Create postmapping with addBorrowing method to save a neu borrowing.

--- microservice-library/src/main/java/com/mimi/mlibrary/web/controllers/BorrowingController.java
+++ microservice-library/src/main/java/com/mimi/mlibrary/web/controllers/BorrowingController.java
@@ -2,11 +2,11 @@ package com.mimi.mlibrary.web.controllers;
 
 import com.mimi.mlibrary.model.source.borrowing.Borrowing;
 import com.mimi.mlibrary.service.borrowing.BorrowingService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -18,13 +18,25 @@ public class BorrowingController {
         this.borrowingService = borrowingService;
     }
 
-    @GetMapping( value = "/Borrowings/MemberId", params = "id")
-    public List<Borrowing> findAllByUserId( @RequestParam int id ) {
+    @GetMapping( value = "/Borrowings/MemberId", params = "id" )
+    public List<Borrowing> findAllByMemberId( @RequestParam int id ) {
         return borrowingService.findByMemberId( id );
     }
 
-    @GetMapping( value = "/Borrowings")
+    @GetMapping( value = "/Borrowings" )
     public List<Borrowing> getAllBorrowings() {
         return borrowingService.findAll();
     }
+
+    @PostMapping( value = "/Borrowings/add" )
+    public ResponseEntity<Void> addBorrowing(@RequestBody Borrowing borrowing ) {
+        Borrowing addedBorrowing = borrowingService.save( borrowing );
+        if( addedBorrowing == null)
+            return ResponseEntity.noContent().build();
+
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand( addedBorrowing.getId() ).toUri();
+        return ResponseEntity.created( location ).build();
+    }
+
+
 }
