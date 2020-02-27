4a026dcd710af1034e0782e7e6effc752828bc71
mimi <lam99@hotmail.fr>
2020-02-27 10:05:44 +0100

    Features:
    Add method to retrieve all authors.

--- microservice-library/src/main/java/com/mimi/mlibrary/service/impl/publication/AuthorServiceImpl.java
+++ microservice-library/src/main/java/com/mimi/mlibrary/service/impl/publication/AuthorServiceImpl.java
@@ -1,6 +1,21 @@
 package com.mimi.mlibrary.service.impl.publication;
 
+import com.mimi.mlibrary.dao.publication.AuthorDao;
+import com.mimi.mlibrary.model.source.publication.Author;
 import com.mimi.mlibrary.service.publication.AuthorService;
 
+import java.util.List;
+
 public class AuthorServiceImpl implements AuthorService {
+
+    private AuthorDao authorDao;
+
+    public AuthorServiceImpl(AuthorDao authorDao) {
+        this.authorDao = authorDao;
+    }
+
+    @Override
+    public List<Author> findAll() {
+        return authorDao.findAll();
+    }
 }
