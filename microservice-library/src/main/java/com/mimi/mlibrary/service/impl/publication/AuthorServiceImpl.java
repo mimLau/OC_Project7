14490753abf72319061343c14c1677c374812421
mimi <lam99@hotmail.fr>
2020-02-27 10:42:39 +0100

    Moved

--- microservice-library/src/main/java/com/mimi/mlibrary/service/impl/publication/AuthorServiceImpl.java
+++ microservice-library/src/main/java/com/mimi/mlibrary/service/impl/publication/AuthorServiceImpl.java
@@ -3,9 +3,11 @@ package com.mimi.mlibrary.service.impl.publication;
 import com.mimi.mlibrary.dao.publication.AuthorDao;
 import com.mimi.mlibrary.model.source.publication.Author;
 import com.mimi.mlibrary.service.publication.AuthorService;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class AuthorServiceImpl implements AuthorService {
 
     private AuthorDao authorDao;
