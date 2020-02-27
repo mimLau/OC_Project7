f482c5d723bf440be4b2efa735dc2f9040719393
mimi <lam99@hotmail.fr>
2020-02-27 20:40:08 +0100

    Features:
    Add findBorrowingById and updateBorrowingReturnDateById methods.

--- microservice-library/src/main/java/com/mimi/mlibrary/service/BorrowingService.java
+++ microservice-library/src/main/java/com/mimi/mlibrary/service/BorrowingService.java
@@ -1,13 +1,16 @@
 package com.mimi.mlibrary.service;
 
-import com.mimi.mlibrary.model.dest.borrowing.BorrowingDto;
 import com.mimi.mlibrary.model.source.borrowing.Borrowing;
+import org.joda.time.LocalDate;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface BorrowingService {
 
+    Borrowing findBorrowingById( int id );
     List<Borrowing> findAll();
     List<Borrowing> findByMemberId( int memberId );
     Borrowing save( Borrowing borrowing );
+    void updateBorrowingReturnDateById( int extendedDay, int id );
 }
