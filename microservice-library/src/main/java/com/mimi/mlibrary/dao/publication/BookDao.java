485ee6e452a26a5a5c70b97c09593eab7749d5cc
mimi <lam99@hotmail.fr>
2020-02-27 09:01:45 +0100

    Feature:
    find all books by author id search.

--- microservice-library/src/main/java/com/mimi/mlibrary/dao/publication/BookDao.java
+++ microservice-library/src/main/java/com/mimi/mlibrary/dao/publication/BookDao.java
@@ -12,8 +12,11 @@ import java.util.List;
 public interface BookDao extends JpaRepository<Book, Integer> {
 
     @Query("SELECT b FROM Book b JOIN FETCH b.author a WHERE a.firstname= :name OR a.lastname= :name or a.alias= :name")
-    List<Book> findAllByAuthor(@Param("name") String name);
+    List<Book> findAllByAuthor( @Param("name") String name );
 
     @Query("SELECT b FROM Book b WHERE b.title LIKE %:title%")
     List<Book> findAllByTitle( @Param("title") String title );
+
+    @Query("SELECT b FROM Book b JOIN FETCH b.author a WHERE a.id= :id")
+    List<Book> findAllByAuthorId( @Param("id") int id );
 }
