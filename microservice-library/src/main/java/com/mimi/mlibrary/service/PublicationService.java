09f6760cd6d074d54ea0ec617bb19110c5989d9e
mimi <lam99@hotmail.fr>
2020-02-27 10:36:32 +0100

    Regroup all Book, Newspaper, Copy, Review methods inside one service.

--- microservice-library/src/main/java/com/mimi/mlibrary/service/PublicationService.java
+++ microservice-library/src/main/java/com/mimi/mlibrary/service/PublicationService.java
@@ -1,7 +1,7 @@
 package com.mimi.mlibrary.service;
 
 import com.mimi.mlibrary.model.dest.publication.BookDto;
-import com.mimi.mlibrary.model.source.publication.Publication;
+import com.mimi.mlibrary.model.source.publication.*;
 import com.mimi.mlibrary.model.dest.publication.PublicationDto;
 
 import java.util.List;
@@ -9,11 +9,39 @@ import java.util.Optional;
 
 public interface PublicationService {
 
-    List<PublicationDto> findAll();
+
+
+    List<Author> findAllAuthor();
+
+    List<Book> findAllBook();
+    List<Book> findAllBookByAuthor( String name );
+    List<Book> findAllBookByTitle( String title );
+    List<Book> findAllBookByAuthorId( int id );
+
+    //List<CopyDto> findAll();
+    List<Copy> findAllCopy();
+    List<Copy> findAllCopyByBookId( int id );
+    List<Copy> findAllCopyByReviewId( int id );
+    List<Copy> findAllCopyByNewspaperId( int id );
+    List<Copy> findAllCopyByDelay();
+
+
+    List<Newspaper> findAllNewspaper();
+    List<Newspaper> findAllNewspaperByName( String name );
+    List<Newspaper> findAllNewspaperByDate( String date );
+
+    List<Review> findAllReview();
+    List<Review> findAllReviewByName( String name );
+    List<Review> findAllReviewByDate( String date );
+
+
+
+
+    /*List<PublicationDto> findAll();
     List<BookDto> findAllBooks();
     Publication findByIsbn( String isbn );
     List<Publication> findAllByAuthor( String name );
     List<PublicationDto> findAllByTitle(String title );
-    Optional<Publication> findById( int id);
+    Optional<Publication> findById( int id);*/
 
 }
