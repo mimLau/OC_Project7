a185b26bf6659bda5f7b6fe0fdcad3adee8a9f17
mimi <lam99@hotmail.fr>
2020-02-27 20:32:09 +0100

    Features:
    Add findBorrowingById and updateBorrowingReturnDateById methods.

--- microservice-library/src/main/java/com/mimi/mlibrary/dao/borrowing/BorrowingDao.java
+++ microservice-library/src/main/java/com/mimi/mlibrary/dao/borrowing/BorrowingDao.java
@@ -1,20 +1,31 @@
 package com.mimi.mlibrary.dao.borrowing;
 
 import com.mimi.mlibrary.model.source.borrowing.Borrowing;
+import org.joda.time.LocalDate;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Repository
 public interface BorrowingDao extends JpaRepository<Borrowing, Integer> {
 
+    @Query("SELECT b FROM Borrowing b WHERE b.id= :id")
+    Borrowing findBorrowingById( @Param("id") int id );
+
     @Query("SELECT b FROM Borrowing  b JOIN FETCH b.member m WHERE m.id= :id")
     List<Borrowing> findByMemberId(@Param("id") int id);
 
     @Query("SELECT b FROM Borrowing  b JOIN FETCH b.copy c WHERE c.id= :id")
-    List<Borrowing> findByCopyId( @Param("id") int id);
+    List<Borrowing> findByCopyId( @Param("id") int id );
+
+    @Transactional
+    @Modifying
+    @Query("Update Borrowing b SET b.returnDate= :newDate WHERE b.id= :id")
+    void  updateBorrowingReturnDateById( @Param("newDate") LocalDate newDate, @Param("id") int id);
 
 }
