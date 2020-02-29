81b2ba9de9921cdfcf9ae284746700349ae6ae90
mimi <lam99@hotmail.fr>
2020-02-29 14:59:18 +0100

    Remove commons attributes with other children class for putting them in parent class.

--- microservice-library/src/main/java/com/mimi/mlibrary/model/source/publication/Review.java
+++ microservice-library/src/main/java/com/mimi/mlibrary/model/source/publication/Review.java
@@ -1,47 +1,12 @@
 package com.mimi.mlibrary.model.source.publication;
 
-import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 import javax.persistence.*;
 import java.io.Serializable;
-import java.util.List;
 
 @Entity
 @DiscriminatorValue("Review")
 public class Review extends Publication implements Serializable {
 
-    private String releaseDate;
-    private String name;
 
-    @JsonManagedReference( value="review_copy")
-    @OneToMany(mappedBy = "review")
-    private List<Copy> copies;
-
-    public Review() {
-        super();
-    }
-
-    public String getReleaseDate() {
-        return releaseDate;
-    }
-
-    public void setReleaseDate(String releaseDate) {
-        this.releaseDate = releaseDate;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public List<Copy> getCopies() {
-        return copies;
-    }
-
-    public void setCopies(List<Copy> copies) {
-        this.copies = copies;
-    }
 }
