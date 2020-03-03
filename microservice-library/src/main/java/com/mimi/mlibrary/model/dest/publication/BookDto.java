2c8510d4790d1d2d30e6831d8b04e1495511ebeb
mimi <lam99@hotmail.fr>
2020-03-03 07:33:25 +0100

    Change type of editor attribute.

--- microservice-library/src/main/java/com/mimi/mlibrary/model/dest/publication/BookDto.java
+++ microservice-library/src/main/java/com/mimi/mlibrary/model/dest/publication/BookDto.java
@@ -1,52 +1,18 @@
 package com.mimi.mlibrary.model.dest.publication;
 
 import com.mimi.mlibrary.model.source.publication.Category;
+import com.mimi.mlibrary.model.source.publication.Editor;
 
 public class BookDto extends PublicationDto {
 
-    private String editor;
-    private String publishingDate;
-    private String title;
-    private String isbn;
-    private Category category;
+    private Editor editor;
 
-    public String getEditor() {
+    public Editor getEditor() {
         return editor;
     }
 
-    public void setEditor(String editor) {
+    public void setEditor(Editor editor) {
         this.editor = editor;
-    }
+    };
 
-    public String getPublishingDate() {
-        return publishingDate;
-    }
-
-    public void setPublishingDate(String publishingDate) {
-        this.publishingDate = publishingDate;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getIsbn() {
-        return isbn;
-    }
-
-    public void setIsbn(String isbn) {
-        this.isbn = isbn;
-    }
-
-    public Category getCategory() {
-        return category;
-    }
-
-    public void setCategory(Category category) {
-        this.category = category;
-    }
 }
