db679f4510c50ec195cb8439e932644d3800f07f
mimi <lam99@hotmail.fr>
2020-03-03 07:50:57 +0100

    -

--- microservice-library/src/main/java/com/mimi/mlibrary/service/impl/PublicationServiceImpl.java
+++ microservice-library/src/main/java/com/mimi/mlibrary/service/impl/PublicationServiceImpl.java
@@ -1,37 +1,129 @@
 package com.mimi.mlibrary.service.impl;
 
 import com.mimi.mlibrary.dao.publication.*;
+import com.mimi.mlibrary.mapper.publication.BookMapper;
+import com.mimi.mlibrary.mapper.publication.PublicationMapper;
+import com.mimi.mlibrary.model.dest.publication.BookDto;
+import com.mimi.mlibrary.model.dest.publication.CopyDto;
+import com.mimi.mlibrary.model.dest.publication.PublicationDto;
 import com.mimi.mlibrary.model.source.publication.*;
 import com.mimi.mlibrary.service.PublicationService;
 import org.joda.time.LocalDate;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class PublicationServiceImpl implements PublicationService {
 
-    /*private PublicationDao publicationDao;
+    private PublicationDao publicationDao;
     private PublicationMapper publicationMapper;
-    private BookMapper bookMapper;*/
+    private BookMapper bookMapper;
+
 
     private AuthorDao authorDao;
-    private BookDao bookDao;
     private CopyDao copyDao;
-    private NewspaperDao newspaperDao;
-    private ReviewDao reviewDao;
 
-    PublicationServiceImpl(AuthorDao authorDao, BookDao bookDao, CopyDao copyDao, NewspaperDao newspaperDao, ReviewDao reviewDao) {
+
+    PublicationServiceImpl(PublicationDao publicationDao, PublicationMapper publicationMapper, AuthorDao authorDao, CopyDao copyDao, BookMapper bookMapper) {
+        this.publicationDao = publicationDao;
+        this.publicationMapper = publicationMapper;
         this.authorDao = authorDao;
-        this.bookDao = bookDao;
         this.copyDao = copyDao;
-        this.newspaperDao = newspaperDao;
-        this.reviewDao = reviewDao;
+        this.bookMapper = bookMapper;
     }
 
 
-    //Author
+
+    /****************
+     * Books
+     * **************/
+
     @Override
+    public List<BookDto> findAllBooks() {
+        List<Book> books = publicationDao.findAllBooks();
+        List<BookDto> bookDtos = bookMapper.map( books );
+        return bookDtos;
+    }
+
+
+    /****************
+     * Newspapers
+     * **************/
+
+    @Override
+    public List<Newspaper> findAllNewspapers() {
+        return publicationDao.findAllNewspapers();
+    }
+
+    @Override
+    public List<Newspaper> findAllNewspapersByDate( LocalDate date ) {
+        //TODO convert date or something like this
+        return publicationDao.findAllNewspaperByDate( date );
+    }
+
+
+
+    /****************
+     * Reviews
+     * **************/
+
+    @Override
+    public List<Review> findAllReviews() {
+        return publicationDao.findAllReviews();
+    }
+
+    @Override
+    public List<Review> findAllReviewsByDate( LocalDate date ) {
+        return publicationDao.findAllReviewsByDate( date );
+    }
+
+
+
+    /****************
+     * Publications
+     * **************/
+
+
+    @Override
+    public Optional<Publication> findPublicationById( int id ) {
+        return publicationDao.findPublicationById( id );
+    }
+
+    @Override
+    public List<Publication> findAllPublications() {
+        return publicationDao.findAllPublications();
+    }
+
+    @Override
+    public List<PublicationDto> findAllByTitle( String title ) {
+        List <PublicationDto> publicationDtos = publicationMapper.map( publicationDao.findAllByTitle( title ));
+        return publicationDtos;
+    }
+
+    @Override
+    public List<Publication> findAllByAuthor( String name ) {
+        return publicationDao.findAllByAuthor( name );
+    }
+
+    @Override
+    public List<Publication> findAllByAuthorId( int id ) {
+        return publicationDao.findAllByAuthorId( id );
+    }
+
+    @Override
+    public Publication findAllByIsbn( String idNb ) {
+        return publicationDao.findAllByIsbn( idNb);
+    }
+
+
+
+    /****************
+     * Authors
+     * **************/
+
+   @Override
     public List<Author> findAllAuthor() {
         return authorDao.findAll();
     }
@@ -47,48 +139,24 @@ public class PublicationServiceImpl implements PublicationService {
     }
 
 
-    //Book
-    @Override
-    public List<Book> findAllBook() {
-        return bookDao.findAll();
-    }
 
-    @Override
-    public List<Book> findAllBookByAuthor( String name ) {
-        return bookDao.findAllByAuthor( name );
-    }
+    /****************
+     * Copies
+     * **************/
 
-    @Override
-    public List<Book> findAllBookByTitle( String title ) {
-        return bookDao.findAllByTitle( title );
-    }
-
-    @Override
-    public List<Book> findAllBookByAuthorId( int id ) {
-        return bookDao.findAllByAuthorId( id );
-    }
-
-
-
-    //Copy
     @Override
     public List<Copy> findAllCopy() {
         return copyDao.findAll();
     }
 
     @Override
-    public List<Copy> findAllCopyByBookId(int bookId ) {
-        return copyDao.findAllByBookId( bookId );
+    public List<CopyDto> findAllCopyDto() {
+        return null;
     }
 
     @Override
-    public List<Copy> findAllCopyByReviewId( int reviewId ) {
-        return copyDao.findAllByBookId( reviewId );
-    }
-
-    @Override
-    public List<Copy> findAllCopyByNewspaperId( int newspaperId ) {
-        return copyDao.findAllByBookId( newspaperId );
+    public List<Copy> findAllCopyByPublicationId( int publicationId ) {
+        return copyDao.findAllCopyByPublicationId( publicationId );
     }
 
     @Override
@@ -98,97 +166,16 @@ public class PublicationServiceImpl implements PublicationService {
         return copyDao.findAllByDelay( localDate );
     }
 
-
-
-    //Newspaper
     @Override
-    public List<Newspaper> findAllNewspaper() {
-        return newspaperDao.findAll();
+    public void updateCopyReturnDateById( LocalDate newDate, int id) {
+        copyDao.updateCopyReturnDateById( newDate, id );
     }
+
 
     @Override
-    public List<Newspaper> findAllNewspaperByName( String name ) {
-        return newspaperDao.findAllByName( name );
-    }
-
-    @Override
-    public List<Newspaper> findAllNewspaperByDate( String date ) {
-
-        //TODO convert date or something like this
-        return newspaperDao.findAllByDate( date );
+    public Copy findCopyById( int id ) {
+        return copyDao.findCopyById( id );
     }
 
 
-    //Review
-
-    @Override
-    public List<Review> findAllReview() {
-        return reviewDao.findAll();
-    }
-
-    @Override
-    public List<Review> findAllReviewByName( String name ) {
-        return reviewDao.findAllByName( name );
-    }
-
-    @Override
-    public List<Review> findAllReviewByDate( String date ) {
-        return reviewDao.findAllByDate( date );
-    }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  /* public List<Publication> findAll() {
-        return publicationDao.findAll();
-    }*/
-
-    /*@Override
-    public List<PublicationDto> findAll() {
-        List<Publication> publications = publicationDao.findAll();
-        List <PublicationDto> publicationDtos = publicationMapper.map(publications );
-
-        return publicationDtos;
-    }
-
-    @Override
-    public List<BookDto> findAllBooks() {
-        List<Book> books = publicationDao.findAllBooks();
-        List <BookDto> bookDtos = bookMapper.map( books );
-        return bookDtos;
-    }
-
-    public Optional<Publication> findById( int id) {
-        return publicationDao.findById( id );
-    }
-
-    @Override
-    public Publication findByIsbn( String isbn ) {
-        return publicationDao.findByIsbn( isbn );
-    }
-
-    @Override
-    public List<Publication> findAllByAuthor( String name ) {
-        return publicationDao.findAllByAuthor( name );
-    }
-
-    @Override
-    /*public List<Publication> findAllByTitle( String title ) {
-        return publicationDao.findAllByTitle( title );
-    }*/
-
-    /*public List<PublicationDto> findAllByTitle( String title ) {
-        List <PublicationDto> publicationDtos = publicationMapper.map( publicationDao.findAllByTitle( title ));
-        return publicationDtos;
-    }*/
 }
