a96255759b1f879ccac35f3a6d268cb0d24ce2c8
mimi <lam99@hotmail.fr>
2020-03-03 07:46:39 +0100

    Feature: Add updateCopyReturnDateById method.

--- microservice-library/src/main/java/com/mimi/mlibrary/dao/publication/CopyDao.java
+++ microservice-library/src/main/java/com/mimi/mlibrary/dao/publication/CopyDao.java
@@ -3,23 +3,34 @@ package com.mimi.mlibrary.dao.publication;
 import com.mimi.mlibrary.model.source.publication.Copy;
 import org.joda.time.LocalDate;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
 public interface CopyDao extends JpaRepository<Copy, Integer> {
 
-    @Query("Select  c FROM Copy c JOIN FETCH c.book b where b.id= :id")
-    List<Copy> findAllByBookId( @Param( "id" ) int id );
+    @Query("Select  c FROM Copy c where c.id= :id")
+    Copy findCopyById( int id );
 
-    @Query("Select  c FROM Copy c JOIN FETCH c.review b where b.id= :id")
-    List<Copy> findAllByReviewId( @Param( "id" ) int id );
-
-    @Query("Select  c FROM Copy c JOIN FETCH c.newspaper b where b.id= :id")
-    List<Copy> findAllByNewspaperId( @Param( "id" ) int id );
+    @Query("Select  c FROM Copy c JOIN FETCH c.publication p where p.id= :id")
+    List<Copy> findAllCopyByPublicationId( @Param( "id" ) int id );
 
     @Query("SELECT c FROM Copy c WHERE c.returnDate < :today")
     List<Copy> findAllByDelay( @Param("today") LocalDate currentDate );
+
+
+    @Transactional
+    @Modifying
+    @Query("Update Copy c SET c.returnDate= :newDate WHERE c.id= :id")
+    void  updateCopyReturnDateById( @Param("newDate") LocalDate newDate, @Param("id") int id );
+
+    @Transactional
+    @Modifying
+    @Query("Update Copy c SET c.available= :available WHERE c.id= :id")
+    void  updateCopyAvailability( @Param("available") boolean available, @Param("id") int id );
+
 }
