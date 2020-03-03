115d72d2eaf22c627d1ceaf43f41074390cf3adf
mimi <lam99@hotmail.fr>
2020-03-03 07:45:14 +0100

    Feature: Add update borrowing status and update borrowing extensionValue methods.

--- microservice-library/src/main/java/com/mimi/mlibrary/service/BorrowingService.java
+++ microservice-library/src/main/java/com/mimi/mlibrary/service/BorrowingService.java
@@ -1,16 +1,18 @@
 package com.mimi.mlibrary.service;
 
 import com.mimi.mlibrary.model.source.borrowing.Borrowing;
-import org.joda.time.LocalDate;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface BorrowingService {
 
-    Borrowing findBorrowingById( int id );
+    Optional<Borrowing> findBorrowingById(int id );
     List<Borrowing> findAll();
     List<Borrowing> findByMemberId( int memberId );
-    Borrowing save( Borrowing borrowing );
-    void updateBorrowingReturnDateById( int extendedDay, int id );
+    Borrowing save( Borrowing borrowing, int memberId, int copyId );
+    void updateBorrowingReturnDateById( int id );
+    void  updateBorrowingExtensionValueById( int id) ;
+    void  updateBorrowingStatus( int borrowingId );
+
 }
