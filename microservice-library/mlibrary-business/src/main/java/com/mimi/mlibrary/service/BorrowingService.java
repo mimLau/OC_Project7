0304aab4f1ecef56b26d0a7b90f690684c9ef3a6
mimi <lam99@hotmail.fr>
2020-03-04 11:57:34 +0100

    Remove optional

--- microservice-library/mlibrary-business/src/main/java/com/mimi/mlibrary/service/BorrowingService.java
+++ microservice-library/mlibrary-business/src/main/java/com/mimi/mlibrary/service/BorrowingService.java
@@ -4,11 +4,10 @@ import com.mimi.mlibrary.model.dest.borrowing.BorrowingDto;
 import com.mimi.mlibrary.model.source.borrowing.Borrowing;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface BorrowingService {
 
-    Optional<BorrowingDto> findBorrowingById(int id );
+    BorrowingDto findBorrowingById(int id );
     List<BorrowingDto> findAll();
     List<BorrowingDto> findByMemberId( int memberId );
     BorrowingDto save( Borrowing borrowing, int memberId, int copyId );
