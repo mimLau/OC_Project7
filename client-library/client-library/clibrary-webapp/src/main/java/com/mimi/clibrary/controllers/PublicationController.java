2c791d123efc3fbe47a4afbdbc2f18d45025675c
mimi <lam99@hotmail.fr>
2020-03-09 10:18:36 +0100

    Feature: create Publication controller in client side.

---
+++ client-library/client-library/clibrary-webapp/src/main/java/com/mimi/clibrary/controllers/PublicationController.java
@@ -0,0 +1,31 @@
+package com.mimi.clibrary.controllers;
+
+
+import org.mimi.clibrary.Beans.publication.PublicationBean;
+import org.mimi.clibrary.proxies.FeignProxy;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@Controller
+public class PublicationController {
+
+    private FeignProxy feignProxy;
+
+    public PublicationController( FeignProxy feignProxy ) {
+        this.feignProxy = feignProxy;
+    }
+
+    @RequestMapping("/Publications")
+    public String publication( Model model) {
+
+        List<PublicationBean> publications = feignProxy.getAllPublications();
+        model.addAttribute( "publications", publications );
+        return "publication";
+    }
+
+
+}
