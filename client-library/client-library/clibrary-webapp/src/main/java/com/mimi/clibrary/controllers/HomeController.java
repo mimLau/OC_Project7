2d53dd6be028bce98cdf1340e475dab7cd909578
mimi <lam99@hotmail.fr>
2020-03-09 10:10:59 +0100

    Feature: create home controller in client side.

---
+++ client-library/client-library/clibrary-webapp/src/main/java/com/mimi/clibrary/controllers/HomeController.java
@@ -0,0 +1,16 @@
+package com.mimi.clibrary.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/")
+    public String homePage( Model model ) {
+        return "home";
+
+    }
+}
