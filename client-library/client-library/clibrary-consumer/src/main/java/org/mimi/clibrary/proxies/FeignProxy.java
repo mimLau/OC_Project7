266e0fe98f1501cf29de241313dd21b3163c0b54
mimi <lam99@hotmail.fr>
2020-03-09 10:10:22 +0100

    Feature: add Feign client as proxy in client side.

---
+++ client-library/client-library/clibrary-consumer/src/main/java/org/mimi/clibrary/proxies/FeignProxy.java
@@ -0,0 +1,30 @@
+package org.mimi.clibrary.proxies;
+
+import org.mimi.clibrary.Beans.account.MemberBean;
+import org.mimi.clibrary.Beans.publication.LibraryBean;
+import org.mimi.clibrary.Beans.publication.PublicationBean;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
+@FeignClient( name = "mlibrary-webservice", url = "localhost:8080")
+public interface FeignProxy {
+
+    @GetMapping( value = "/Members" )
+    List<MemberBean> getAllMembers();
+
+    @GetMapping( value = "/Members", params = "id" )
+    MemberBean getMemberById(@RequestParam("id") int id );
+
+    @GetMapping(value = "/Publications")
+    List<PublicationBean> getAllPublications();
+
+    @GetMapping(value = "/Publications", params = "id")
+    PublicationBean getPublicationsById(@RequestParam("id") int id);
+
+    @GetMapping(value = "/Libraries")
+    List<LibraryBean> getAllLibraries();
+
+}
