93a9a7b69802faa8cd00da24768c37280558385e
mimi <lam99@hotmail.fr>
2020-03-10 11:43:52 +0100

    Feature: add request to get a user by email and pass

--- client-library/client-library/clibrary-consumer/src/main/java/org/mimi/clibrary/proxies/FeignProxy.java
+++ client-library/client-library/clibrary-consumer/src/main/java/org/mimi/clibrary/proxies/FeignProxy.java
@@ -9,20 +9,27 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
-@FeignClient( name = "mlibrary-webservice", url = "localhost:8080")
+@FeignClient( name = "mlibrary-webservice", url = "localhost:8080" )
 public interface FeignProxy {
 
     @GetMapping( value = "/Members" )
     List<MemberBean> getAllMembers();
 
+    @GetMapping( value ="/Members", params = { "mail", "pass" } )
+    MemberBean getMemberByNameAndPass(@RequestParam("mail") String mail, @RequestParam("pass") String pass );
+
     @GetMapping( value = "/Members", params = "id" )
     MemberBean getMemberById(@RequestParam("id") int id );
+
+
 
     @GetMapping(value = "/Publications")
     List<PublicationBean> getAllPublications();
 
     @GetMapping(value = "/Publications", params = "id")
     PublicationBean getPublicationsById(@RequestParam("id") int id);
+
+
 
     @GetMapping(value = "/Libraries")
     List<LibraryBean> getAllLibraries();
