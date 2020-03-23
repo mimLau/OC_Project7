1e74f2df0d041c390a575da41c890d1f0a88cc2e
mimi <lam99@hotmail.fr>
2020-03-23 12:15:03 +0100

    Feat: add home  Controller

--- client-library/client-library/clibrary-webapp/src/main/java/com/mimi/clibrary/controllers/HomeClientController.java
+++ client-library/client-library/clibrary-webapp/src/main/java/com/mimi/clibrary/controllers/HomeClientController.java
@@ -3,14 +3,13 @@ package com.mimi.clibrary.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 public class HomeClientController {
 
     @RequestMapping("/")
     public String homePage( Model model ) {
-        return "home";
+        return "userHome";
 
     }
 }
