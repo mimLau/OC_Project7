6118adfd2cf8c3f258fde64af99ea810e25b802c
mimi <lam99@hotmail.fr>
2020-05-02 09:26:30 +0200

    Delete unused imports.

--- microservice-library/mlibrary-consumer/src/main/java/com/mimi/mlibrary/mapper/publication/SubCategoryMapper.java
+++ microservice-library/mlibrary-consumer/src/main/java/com/mimi/mlibrary/mapper/publication/SubCategoryMapper.java
@@ -1,17 +1,15 @@
 package com.mimi.mlibrary.mapper.publication;
 
-import com.mimi.mlibrary.model.entity.publication.SubCategory;
 import org.mapstruct.Qualifier;
-import org.springframework.context.annotation.Bean;
-import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Service;
 
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-
+/**
+ * Custom mapper to retrieve the String value of Subcategory enum.
+ */
 @Qualifier
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.CLASS)
